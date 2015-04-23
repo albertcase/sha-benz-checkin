0ce05c7c6784964b2b3db5d08c21b4065005a6c8
demonzhang <demon.zhang@samesamechina.com>
2015-04-23 15:43:30 +0800

    ssss

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@
     $(function(){
      
    		$(".submit_btn").on("click", function(){
+            alert(1)
             var file=$("#file").val();
             if(file==""||file=="请输入您的姓名"){
-            	
+                alert(1)
+            	$.ajax({
+                    "url":"./Request.php",
+                    "type":"post",
+                    "data":{"model":"search","cardnum":"123"},
+                    "dataType":"json",
+                    "success":function(data){
+                        alert(data.code)
+                    }
+                });
             	return false;
             }
         });
