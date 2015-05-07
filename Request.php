42082e7fd1620fe44b2688505fd9809336a3366c
demonzhang <demon.zhang@samesamechina.com>
2015-05-07 18:41:13 +0800

    no message

--- Request.php
+++ Request.php
@@ -36,7 +36,7 @@ if(isset($_POST['model'])){
 				print json_encode(array("code"=>2,"msg"=>"请填写必填项"));
 				exit;
 			}
-			$sql="select * from  user where name like '%".$cardnum."%'";
+			$sql="select * from  user where chinese like '%".$cardnum."%' or english like '%".$cardnum."%'";
 			$rs=$db->getAll($sql,true);
 			print json_encode(array("code"=>1,"msg"=>$rs));
 			exit;
